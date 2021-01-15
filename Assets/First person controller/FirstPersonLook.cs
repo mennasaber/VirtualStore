c4a0d55eb0291978953600b17562ea7bb6ed6299
mennasaber <mennasaber0193@gmail.com>
2021-01-15 22:32:35 +0200

    Initial Env.

---
+++ Assets/First person controller/FirstPersonLook.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class FirstPersonLook : MonoBehaviour
+{
+    [SerializeField]
+    Transform character;
+    public FixedTouchField TouchField;
+    protected float CameraAngle;
+    protected float CameraAngleSpeed = 0.2f;
+    void Reset()
+    {
+        character = GetComponentInParent<FirstPersonMovement>().transform;
+    }
+    void Update()
+    {
+        CameraAngle += TouchField.TouchDist.x * CameraAngleSpeed;
+        character.rotation = Quaternion.AngleAxis(CameraAngle, Vector3.up) * transform.rotation;
+        CameraAngle = 0;
+    }
+}
---
+++ Assets/First person controller/FirstPersonMovement.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class FirstPersonMovement : MonoBehaviour
+{
+    public float speed = 5;
+    Vector2 velocity;
+    public Joystick joystick; 
+    private float verticalMove = 0f;
+    private float horizontalMove = 0f;
+    void FixedUpdate()
+    {
+        Debug.Log(joystick.Horizontal);
+        if (joystick.Horizontal > 0.80)
+            horizontalMove = -1;
+        else if ( joystick.Horizontal < -0.80)
+            horizontalMove = 1;
+        else if (joystick.Vertical > 0.80)
+            verticalMove = -1;
+        else if (joystick.Vertical < -0.80)
+            verticalMove = 1;
+
+        velocity.y = verticalMove * speed * Time.deltaTime;
+        velocity.x = horizontalMove * speed * Time.deltaTime;
+        transform.Translate(velocity.x, 0, velocity.y);
+        verticalMove = 0;
+        horizontalMove = 0;
+    }
+}
